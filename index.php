5bb01d931bfea190baa86f903baa0d4dd9125154
karin valcanover <valcanoverkarin@outlook.it>
2021-05-31 04:10:21 +0200

    fine esercizio

--- index.php
+++ index.php
@@ -29,3 +29,42 @@
     $user2 = new User('Alessia', 'Ferrari', '31', 'aleferrari83@gmail.com');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+
+    <h1>Users</h1>
+
+    <ul>
+        <li>
+            <h3>user 0</h3>
+            Name: <?php echo $user0->name; ?> <br>
+            Lastname: <?php echo $user0->lastname; ?> <br>
+            Age: <?php echo $user0->age; ?> <br>
+            Email: <?php echo $user0->email; ?> <br>
+        </li>
+
+        <li>
+            <h3>user 1</h3>
+            Name: <?php echo $user1->name; ?> <br>
+            Lastname: <?php echo $user1->lastname; ?> <br>
+            Age: <?php echo $user1->age; ?> <br>
+            Email: <?php echo $user1->email; ?> <br>
+        </li>
+
+        <li>
+            <h3>user 2</h3>
+            Name: <?php echo $user2->name ?> <br>
+            Lastname: <?php echo $user2->lastname ?> <br>
+            Age: <?php echo $user2->age ?> <br>
+            Email: <?php echo $user2->email ?> <br>
+        </li>
+    </ul>
+</body>
+</html>
